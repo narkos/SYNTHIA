4cf4d68505028aea7b5018c988690b35b1551272
narkos <narkos.newb@gmail.com>
2018-08-27 09:16:58 +0200

    maybe skater?

---
+++ SYNTHIA_GAME/Assets/Scripts/PLAYER/SKATER/SkateMainController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkateMainController : MonoBehaviour {
+
+	[SerializeField] private SkateBoardController _boardController;
+	[SerializeField] private SkateCharController _charController;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
